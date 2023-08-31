a58aa79023495411de62b1f1a0dba50ee1c50d12
-Arman.Aghighi <arman.aghighi@gmail.com>
2023-08-31 15:30:33 +0430

    Resource File , Animation Bug Fixed , Score count , Sort and search optimization

--- AlphaBettySaga/Assets/Scripts/OnStartAnimation.cs
+++ AlphaBettySaga/Assets/Scripts/OnStartAnimation.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.IO;
 public class OnStartAnimation : MonoBehaviour
 {
     private bool _isStarted = false;
@@ -9,16 +11,19 @@ public class OnStartAnimation : MonoBehaviour
     [SerializeField] private string _name;
     [SerializeField] private int _value;
     private bool _isCorrectWord = false;
-    private int _wordLength = 0;
     private Text _showText;
     private Text _scoreText;
+    private Text _remainMoveText;
+    private int _remainMove = 8;
     private bool _isSelected = false;
     private int _score = 0;
     private void Awake()
     {
         _showText = GameObject.FindGameObjectWithTag("Show-Word").GetComponent<Text>();
         _scoreText = GameObject.FindGameObjectWithTag("Score-Word").GetComponent <Text>();
+        _remainMoveText = GameObject.FindGameObjectWithTag("RemainMoveText").GetComponent<Text>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        PlayerPrefs.SetInt("Score", 0);
     }
     private void Start()
     {
@@ -31,6 +36,17 @@ public class OnStartAnimation : MonoBehaviour
         {
             StartCoroutine(Animation());
             _isStarted = true;
+        }
+    }
+    private bool IsSelectable(int _parentName)
+    {
+        if ((transform.parent.gameObject.name == (_parentName + 1).ToString()) || (transform.parent.gameObject.name == (_parentName - 1).ToString()))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
     private IEnumerator Animation()
@@ -52,45 +68,81 @@ public class OnStartAnimation : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            OnSelectManager();
+                OnSelectManager();
         }
         if (Input.GetMouseButtonUp(0))
         {
+            if(_showText.text != "")
+            {
+                if (CheckCorrectWord(_showText.text))
+                    _isCorrectWord = true;
+                else
+                    _isCorrectWord = false;
+            }
             OnDeselectManager();
         }
     }
     private void OnSelectManager()
     {
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-        if (hit.collider != null && hit.collider.gameObject == gameObject)
+        if (hit.collider != null && hit.collider.gameObject == gameObject && _showText.text.Length <= 7)
         {
-            int _selectedIndex = int.Parse(transform.parent.gameObject.name);
-            if (!_isSelected && _wordLength <= 8)
+            if (!_isSelected)
             {
-                Debug.Log(_selectedIndex);
+                PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("_Score") + _value);
                 _spriteRenderer.color = Color.green;
                 _showText.text += _name;
-                _wordLength = _showText.text.Length;
                 _isSelected = true;
-                _score += _value;
+                gameObject.tag = "isSelected";
             }
+        }
+        else
+        {
+            _isCorrectWord = false;
+        }
+    }
+    private bool CheckCorrectWord(string _newName)
+    {
+        string filePath = "D:/Git/ArmanAghighi_AlphaBettySaga/AlphaBettySaga/Assets/Resources/Resource.txt";
+        if (File.Exists(filePath))
+        {
+            StreamReader reader = new StreamReader(filePath);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                // Check if the line length matches the _newName word length
+                if (line.Length == _newName.Length && line.ToLower().Trim().Contains(_newName.ToLower().Trim()))
+                {
+                    //Debug.Log("Word Found");
+                    reader.Close();
+                    return true;
+                }
+            }
+            reader.Close();
+            //Debug.Log("Word Not Found");
+            return false;
+        }
+        else
+        {
+            return false;
         }
     }
     private void OnDeselectManager()
     {
         if (!_isCorrectWord)
         {
+            gameObject.tag = "Untagged";
             _spriteRenderer.color = Color.white;
             _isSelected = false;
             _showText.text = "";
         }
         else
         {
-            _score *= 10;
-            _score += _showText.text.Length;
+            _remainMove--;
+            _remainMoveText.text = _remainMove.ToString();
+
             _isSelected = false;
             _showText.text = "";
-            _scoreText.text = _score.ToString();
         }
     }
 }
